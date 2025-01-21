09dd400cbad6bbd31a3eb8d9cf093246687c817f
Andrew <71823699+riezong@users.noreply.github.com>
2025-01-21 15:11:08 +1100

    Add displayLibrary and addBookToLibrary functions

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const myLibrary = [];
+
 function Book(title, author, noOfPages, readStatus) {
     this.title = title;
     this.author = author;
     this.noOfPages = noOfPages;
     this.readStatus = readStatus;
     this.info = title + " by " + author + ", " + noOfPages + ", " + readStatus;
-  }
+}
   
-  const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
-  console.log(theHobbit.info); // 'steve'
-  
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayLibrary() {
+    for (const book of myLibrary) {
+        console.log(book.info);        
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+addBookToLibrary(theHobbit);
+
+const harryPotter = new Book('Harry Potter 1', 'J.K. Rowling', '223 pages', 'read');
+addBookToLibrary(harryPotter);
+
+displayLibrary();
