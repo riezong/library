c88e72ed7e46e1e86d8e248198fb21d10b402cb9
Andrew <71823699+riezong@users.noreply.github.com>
2025-01-27 12:23:52 +1100

    Defer script and print Library in DOM

--- script.js
+++ script.js
@@ -13,9 +13,13 @@ function addBookToLibrary(book) {
 }
 
 function displayLibrary() {
+    const bookList = [];
     for (const book of myLibrary) {
-        console.log(book.info);        
+        // console.log(book.info);
+        bookList.push(book.info);
     }
+    console.log(bookList);
+    return bookList;
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
@@ -25,3 +29,12 @@ const harryPotter = new Book('Harry Potter 1', 'J.K. Rowling', '223 pages', 'rea
 addBookToLibrary(harryPotter);
 
 displayLibrary();
+
+const container = document.querySelector(".container");
+const div = document.createElement("div");
+
+let showLibrary = document.createElement("p");
+showLibrary.textContent = displayLibrary();
+
+div.appendChild(showLibrary);
+container.appendChild(div);
