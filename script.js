56e467913392db9435266e668215576588337eb5
Andrew <71823699+riezong@users.noreply.github.com>
2025-01-28 15:14:42 +1100

    Add delete button to each card that removes entry

--- script.js
+++ script.js
@@ -57,10 +57,18 @@ function printLibrary() {
         readStatus.classList.add("readStatus");
         readStatus.textContent = myLibrary[i].readStatus;
 
+        let deleteEntry = document.createElement("button");
+        deleteEntry.classList.add("deleteEntry");
+        deleteEntry.textContent = "Delete";
+        deleteEntry.addEventListener("click", () => {
+            card.remove();
+        });
+
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(noOfPages);
         card.appendChild(readStatus);
+        card.appendChild(deleteEntry);
         library.appendChild(card);
     }
 }
@@ -70,24 +78,31 @@ function refreshLibrary() {
     printLibrary();
 }
 
-
-const showButton = document.querySelector("#openDialog");
-const closeButton = document.querySelector("#closeDialog");
-// "Show the dialog" button opens the dialog modally
-showButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-    dialog.close();
-});
-
 // Example books
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet');
 addBookToLibrary('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', '223', 'read');
 addBookToLibrary('Dune', 'Frank Herbert', '896', 'read');
 
 printLibrary();
+
+// Buttons
+const showButton = document.querySelector("#openDialog");
+const closeButton = document.querySelector("#closeDialog");
+// const deleteEntryButton = document.querySelector(".deleteEntry");
+// "Add Book" button opens the dialog modally
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+// "X" button closes the dialog
+closeButton.addEventListener("click", () => {
+    dialog.close();
+});
+// // "Delete" button deletes book from library
+// deleteEntryButton.addEventListener("click", () => {
+//     const library = document.querySelector(".library");
+//     const card = document.querySelector(".card");
+//     library.removeChild(card);
+// });
 
 // Get a reference to the form element
 const form = document.getElementById("bookForm");
